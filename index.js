6d59bbcb814338a890218bb3062304801b703b4b
Peol13 <86568486+Peol13@users.noreply.github.com>
2021-07-29 14:42:19 +0200

    Add tic tac toe in new version

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-class Game {
-  fields;
-  activePlayer;
-  gameActive;
+window.onload = function () {
+  app.init();
+};
 
-  currentMode = null;
-  doesAIMoveFirst = false;
-
-  winningConditions = [
+let easyMode = true;
+class App {
+  winningVariants = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -14,186 +12,128 @@ class Game {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [6, 4, 2],
+    [2, 4, 6],
   ];
 
-  constructor() {
-    this.board = new Board(
-      this.handleItemClick,
-      this.handleReset,
-      this.handleModeChange
-    );
-    this.setDefaults();
+  currentPlayer = "O";
+
+  init() {
+    pvp.addEventListener("click", () => this.changePvp());
+    easy.addEventListener("click", () => this.changeEasy());
+    document
+      .querySelectorAll(".cell")
+      .forEach((cell) => cell.addEventListener("click", this.cellClick));
+    document
+      .getElementById("restart-game")
+      .addEventListener("click", () => this.restartGame());
+    if (easyMode == true) {
+      this.AITurn();
+    }
   }
-  validateGame = () => {
-    let gameWin = false;
-    for (let i = 0; i <= 7; i++) {
-      const [posA, posB, posC] = this.winningConditions[i];
-      const value1 = this.fields[posA];
-      const value2 = this.fields[posB];
-      const value3 = this.fields[posC];
 
-      if (value1 !== "" && value1 === value2 && value1 === value3) {
-        gameWin = true;
-        break;
-      }
-    }
-    if (gameWin) {
-      this.gameActive = false;
-      this.board.displayWinMessage(this.activePlayer);
-    } else if (this.isBoardFull()) {
-      this.gameActive = false;
-      this.board.displayTieMessage();
-    }
-  };
-  isBoardFull = () => {
-    return this.fields.find((field) => field === "") === undefined;
-  };
-  handleModeChange = (e) => {
-    this.currentMode = this.getModeClassForName(e.target.value);
-    this.setDefaults(false);
-    this.board.resetBoard();
-  };
-  getModeClassForName = (name) => {
-    if (name === "easy") return new EasyMode();
-    if (name === "medium") return new MediumMode();
-    return null;
-  };
-  handleReset = () => {
-    this.setDefaults(!this.doesAIMoveFirst);
-    this.AIsFirstMove();
-  };
-  AIsFirstMove = () => {
-    if (this.doesAIMoveFirst && this.currentMode !== null) {
-      this.makeMove(this.currentMode.getMove(this.fields, this.activePlayer));
-    }
+  cellClick = (e) => {
+    this.playerTurn(e.target);
   };
 
-  handleItemClick = (e) => {
-    const { pos } = e.target.dataset;
+  innitGame() {
+    this.currentPlayer = "X";
 
-    if (this.gameActive && this.fields[pos] === "") {
-      this.makeMove(pos);
-
-      if (this.gameActive && this.currentMode !== null) {
-        this.makeMove(this.currentMode.getMove(this.fields, this.activePlayer));
-      }
-    }
-  };
-
-  makeMove = (position) => {
-    this.fields[position] = this.activePlayer;
-    this.board
-      .getFieldForPositoin(position)
-      .classList.add(`board__item--filled-${this.activePlayer}`);
-    this.validateGame();
-
-    this.activePlayer = this.activePlayer === "X" ? "O" : "X";
-    this.board.setCuttentPlayer(this.activePlayer);
-  };
-  setDefaults = (isAIsMove) => {
-    this.fields = ["", "", "", "", "", "", "", "", ""];
-    this.activePlayer = "X";
-    this.gameActive = true;
-    this.doesAIMoveFirst = isAIsMove !== undefined ? isAIsMove : false;
-  };
-}
-
-class Board {
-  fieldsElements = document.querySelectorAll(".board__item");
-  panel = document.querySelector(".panel");
-  button = document.querySelector(".restart-button");
-  modeSelect = document.querySelector("#mode-select");
-  currentPlayer = document.querySelector("#current-player");
-
-  constructor(onItemClick, onButtonClick, onModeChange) {
-    this.onButtonClick = onButtonClick;
-
-    this.button.addEventListener("click", this.handleButtonClick);
-    this.fieldsElements.forEach((field) => {
-      field.addEventListener("click", onItemClick);
+    document.querySelectorAll(".cell").forEach((el) => {
+      el.innerHTML = "";
     });
-    this.modeSelect.addEventListener("change", onModeChange);
-    this.setCuttentPlayer("X");
   }
-  setCuttentPlayer = (player) => {
-    this.currentPlayer.innerText = player;
-  };
-  handleButtonClick = () => {
-    this.resetBoard();
-    this.onButtonClick();
-  };
-  resetBoard = () => {
-    this.resetBoardClasses();
-    this.clearMessage();
-    this.setCuttentPlayer("X");
-  };
-  resetBoardClasses = () => {
-    this.fieldsElements.forEach((field) => {
-      field.classList.remove("board__item--filled-X", "board__item--filled-O");
-    });
-  };
+  playerTurn(el) {
+    if (el.innerHTML == "X" || el.innerHTML == "O") return;
+    el.innerHTML = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer == "X" ? "O" : "X";
+    this.checkWinner();
 
-  getFieldForPositoin = (position) => {
-    return this.fieldsElements[position];
-  };
-  displayWinMessage = (activePlayer) => {
-    this.panel.innerText = `Gratulacje ${activePlayer}, Wygrałeś!`;
-  };
+    if (this.currentPlayer == "O" && easyMode == true) {
+      this.AITurn();
+      this.checkWinner();
+    }
+  }
 
-  displayTieMessage = () => {
-    this.panel.innerText = "Remis!";
-  };
+  AITurn() {
+    this.checkWinner();
+    let SetNr = Math.floor(Math.random() * 9);
+    let AddInCellSetNr = document.querySelector(
+      `[data-index="${SetNr}"]`
+    ).innerHTML;
 
-  clearMessage = () => {
-    this.panel.innerText = "";
-  };
-}
+    //console.log(document.querySelector(`[data-index="${SetNr}"]`)) - Index pola wylosowanego przez AI
 
-const game = new Game();
-
-class EasyMode {
-  getMove = (fields, player) => {
-    const emptyIndexes = Object.entries(fields)
-      .filter((fieldsEntry) => fieldsEntry[1] === "")
-      .map((fieldsEntry) => fieldsEntry[0]);
-    const randomPositionIndex = Math.floor(Math.random() * emptyIndexes.length);
-    return emptyIndexes[randomPositionIndex];
-  };
-}
-winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2],
-];
-class MediumMode {
-  getMove = (fields, player) => {
-    for (let i = 0; i <= 7; i++) {
-      const [posA, posB, posC] = winningConditions[i];
-      const value1 = fields[posA];
-      const value2 = fields[posB];
-      const value3 = fields[posC];
-
-      if (value1 === value2 && value1 !== "" && value3 === "") {
-        return posC;
-      }
-      if (value1 === value3 && value1 !== "" && value2 === "") {
-        return posB;
-      }
-      if (value2 === value3 && value2 !== "" && value1 === "") {
-        return posA;
-      }
+    if (AddInCellSetNr == "") {
+      document.querySelector(`[data-index="${SetNr}"]`).innerHTML = "O";
+      this.currentPlayer = "X";
     }
 
-    const emptyIndexes = Object.entries(fields)
-      .filter((fieldsEntry) => fieldsEntry[1] === "")
-      .map((fieldsEntry) => fieldsEntry[0]);
-    const randomPositionIndex = Math.floor(Math.random() * emptyIndexes.length);
-    return emptyIndexes[randomPositionIndex];
-  };
+    if (AddInCellSetNr !== "") {
+      this.AITurn();
+    }
+  }
+  cell1 = document.querySelector(`[data-index="0"]`);
+  cell2 = document.querySelector(`[data-index="1"]`);
+  cell3 = document.querySelector(`[data-index="2"]`);
+  cell4 = document.querySelector(`[data-index="3"]`);
+  cell5 = document.querySelector(`[data-index="4"]`);
+  cell6 = document.querySelector(`[data-index="5"]`);
+  cell7 = document.querySelector(`[data-index="6"]`);
+  cell8 = document.querySelector(`[data-index="7"]`);
+  cell9 = document.querySelector(`[data-index="8"]`);
+
+  checkWinner() {
+    for (let i = 0; i < this.winningVariants.length; i++) {
+      const variant = this.winningVariants[i];
+      const a = this.getCellValue(variant[0]);
+      const b = this.getCellValue(variant[1]);
+      const c = this.getCellValue(variant[2]);
+
+      if (a == "" || b == "" || c == "") continue;
+      if (a == b && b == c) {
+        this.setWinner(" - zwyciężył: " + a);
+      }
+
+      if (
+        this.cell1.innerHTML !== "" &&
+        this.cell2.innerHTML !== "" &&
+        this.cell3.innerHTML !== "" &&
+        this.cell4.innerHTML !== "" &&
+        this.cell5.innerHTML !== "" &&
+        this.cell6.innerHTML !== "" &&
+        this.cell7.innerHTML !== "" &&
+        this.cell8.innerHTML !== "" &&
+        this.cell9.innerHTML !== "" &&
+        a !== b &&
+        b !== c
+      ) {
+        this.setWinner(" - Remis");
+      }
+    }
+  }
+
+  restartGame() {
+    this.innitGame();
+    this.init();
+    this.setWinner("");
+  }
+  changePvp() {
+    easyMode = false;
+    this.innitGame();
+    this.init();
+  }
+  changeEasy() {
+    easyMode = true;
+    this.innitGame();
+    this.init();
+  }
+  setWinner(str) {
+    document.getElementById("winner").innerHTML = str;
+  }
+  getCellValue(index) {
+    return document.querySelector(`.cell[data-index='${index}']`).innerHTML;
+  }
 }
+const app = new App();
+let pvp = document.querySelector(".pvp");
+let easy = document.querySelector(".easy");
